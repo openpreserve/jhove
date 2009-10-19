619fbfc8804e313219fdbc5565c5912f10b56506
Gary McGath <developer@mcgath.com>
2009-10-19 19:31:55 +0000

    Update copyright

--- classes/edu/harvard/hul/ois/jhove/PropertyType.java
+++ classes/edu/harvard/hul/ois/jhove/PropertyType.java
@@ -1,6 +1,6 @@
 /**********************************************************************
  * Jhove - JSTOR/Harvard Object Validation Environment
- * Copyright 2003 by JSTOR and the President and Fellows of Harvard College
+ * Copyright 2003-2009 by JSTOR and the President and Fellows of Harvard College
  **********************************************************************/
 
 package edu.harvard.hul.ois.jhove;
