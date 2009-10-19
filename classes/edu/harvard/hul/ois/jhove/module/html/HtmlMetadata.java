4bd57465d1b1353e456878bde04b56d5b5085fea
Gary McGath <developer@mcgath.com>
2009-10-19 19:26:41 +0000

    Update copyright date

--- classes/edu/harvard/hul/ois/jhove/module/html/HtmlMetadata.java
+++ classes/edu/harvard/hul/ois/jhove/module/html/HtmlMetadata.java
@@ -1,6 +1,6 @@
 /**********************************************************************
  * Jhove - JSTOR/Harvard Object Validation Environment
- * Copyright 2004 by JSTOR and the President and Fellows of Harvard College
+ * Copyright 2004-2009 by JSTOR and the President and Fellows of Harvard College
  **********************************************************************/
 
 package edu.harvard.hul.ois.jhove.module.html;
