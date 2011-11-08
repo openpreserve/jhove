ffb41a5e80b19a931a02a7378d50f9bd072526f6
Carol Chou <carolchou@users.sourceforge.net>
2011-11-08 19:39:52 +0000

    escape & character in text, address bug #3386225

--- classes/edu/harvard/hul/ois/jhove/HandlerBase.java
+++ classes/edu/harvard/hul/ois/jhove/HandlerBase.java
@@ -620,6 +620,12 @@ public abstract class HandlerBase
 		n--;
 	    }
 	}
+	    // [CC], escape & which is disallowed in xml
+        n = 0;
+        while ((n = buffer.indexOf ("&", n)) > -1) {
+            buffer.insert (n+1, "amp;");
+            n +=5;
+        }
 	n = 0;
         while ((n = buffer.indexOf ("\"", n)) > -1) {
             // [LP] fix for invalid escaping, "" quotes were accidentally left in place.
