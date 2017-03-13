2443e2b4a0bea4a5624f62dcf8084948c7988f09
Carl Wilson <carl@openplanetsfoundation.org>
2017-03-13 22:23:47 +0000

    FIX - Fixed and tidied JHOVE test scripts - fixed scripts to use variables for version; - improved reusability of update script; and - tightened tidy ups for less repetition.

--- jhove-bbt/scripts/travis-test.sh
+++ jhove-bbt/scripts/travis-test.sh
@@ -1,42 +1,43 @@
 #!/usr/bin/env bash
-
+# Grab the execution directory
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export SCRIPT_DIR
 
 # Include utils script
-. "$SCRIPT_DIR/inc/bb-utils.sh"
+. "${SCRIPT_DIR}/inc/bb-utils.sh"
+# TEST_ROOT directory default
+TEST_ROOT="./test-root"
+TEST_BASELINES_ROOT="${TEST_ROOT}/baselines"
+TEST_INSTALL_ROOT="${TEST_ROOT}/jhove"
+CANDIADATE_ROOT="${TEST_ROOT}/candidates"
+TARGET_ROOT="${TEST_ROOT}/targets"
+BASELINE_VERSION=11
+
+# Create the JHOVE test root if it doesn't exist
+[[ -d "${TEST_ROOT}" ]] || mkdir -p "${TEST_ROOT}"
+# Create the JHOVE test installation root if it doesn't exist
+[[ -d "${TEST_INSTALL_ROOT}" ]] || mkdir -p "${TEST_INSTALL_ROOT}"
+# Create the JHOVE baseline installation root if it doesn't exist
+[[ -d "${TEST_BASELINES_ROOT}" ]] || mkdir -p "${TEST_BASELINES_ROOT}"
 
 # Install v1.11 and create test baseline
-if [ -d test-root/jhove ]; then
-	rm -rf test-root/jhove
-fi
-mkdir -p test-root/jhove
+bash "${SCRIPT_DIR}/legacy-baselines.sh" -d "${TEST_ROOT}" -v "${BASELINE_VERSION}"
+# Create the JHOVE baseline installation root if it doesn't exist
+[[ -d "${CANDIADATE_ROOT}" ]] || mkdir -p "${CANDIADATE_ROOT}"
 
-if [ -d test-root/baselines ]; then
-	rm -rf test-root/baselines
-fi
-mkdir -p test-root/baselines
-
-bash "$SCRIPT_DIR/legacy-baselines.sh"
-
-if [ -d test-root/candidates ]; then
-	rm -rf test-root/candidates
-fi
-mkdir -p test-root/candidates/1.14
-
+# Set up the temp install location for JHOVE development
 tempInstallLoc="/tmp/to-test";
 if [[ -d "${tempInstallLoc}" ]]; then
 	rm -rf "${tempInstallLoc}"
 fi
+
+# Create the test target root if it doesn't exist
+[[ -d "${TARGET_ROOT}" ]] || mkdir -p "${TARGET_ROOT}"
+
+# Grab the Major and Minor versions from the full Maven project version string
 MVN_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
-installJhoveFromFile "./jhove-installer/target/jhove-xplt-installer-${MVN_VERSION}.jar" "${tempInstallLoc}"
-bash "$SCRIPT_DIR/baseline-jhove.sh" -j "${tempInstallLoc}" -c test-root/corpora -o test-root/candidates/1.14
+MAJOR_MINOR_VER="${MVN_VERSION%.*}"
 
-if [ -d test-root/targets ]; then
-	rm -rf test-root/targets
-fi
-mkdir -p test-root/targets/1.14
-
-bash "$SCRIPT_DIR/create-1-14-target.sh"
-bash "$SCRIPT_DIR/bbt-jhove.sh" -b test-root/targets/1.14/release -c ./test-root/corpora -j ./ -o test-root/candidates -k dev-test -i
+bash "${SCRIPT_DIR}/create-${MAJOR_MINOR_VER}-target.sh" -b "1.${BASELINE_VERSION}" -c "${MAJOR_MINOR_VER}"
+bash "${SCRIPT_DIR}/bbt-jhove.sh" -b "${TEST_ROOT}/targets/${MAJOR_MINOR_VER}" -c "${TEST_ROOT}/corpora" -j . -o "${TEST_ROOT}/candidates" -k "dev-${MAJOR_MINOR_VER}" -i
 exit $?
