9969f6b7ef91dfd6b7e4e751546fb03626a179a8
Thomas Ledoux <tledouxfr@gmail.com>
2017-04-20 17:15:23 +0200

    Generate valid mix and textMD metadata Add unit test for all the outputs. Fixes #219

---
+++ jhove-core/src/test/java/edu/harvard/hul/ois/jhove/handler/XmlHandlerTest.java
@@ -0,0 +1,177 @@
+package edu.harvard.hul.ois.jhove.handler;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import edu.harvard.hul.ois.jhove.NisoImageMetadata;
+import edu.harvard.hul.ois.jhove.Rational;
+import edu.harvard.hul.ois.jhove.TextMDMetadata;
+
+@RunWith(JUnit4.class)
+public class XmlHandlerTest {
+    private static final Logger LOGGER = Logger.getLogger (XmlHandlerTest.class.getName());
+
+	File outputFile;
+	PrintWriter writer;
+	XmlHandler handler;
+
+	protected static NisoImageMetadata TEST_NISO_IMAGE_MD;
+	protected static TextMDMetadata TEST_TEXTMD;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws IOException {
+		// Define the test instance for NisoImageMetadata to be serialized
+		TEST_NISO_IMAGE_MD = new NisoImageMetadata();
+		TEST_NISO_IMAGE_MD.setByteOrder(NisoImageMetadata.BYTEORDER[1]);
+		TEST_NISO_IMAGE_MD.setCompressionScheme(1);
+		TEST_NISO_IMAGE_MD.setImageWidth(8192);
+		TEST_NISO_IMAGE_MD.setImageLength(8192);
+		TEST_NISO_IMAGE_MD.setColorSpace(2);
+		Rational r0 = new Rational(0, 1);
+		Rational r255 = new Rational(255, 1);
+		TEST_NISO_IMAGE_MD.setReferenceBlackWhite(new Rational[] { r0, r255, r0,
+				r255, r0, r255 });
+		TEST_NISO_IMAGE_MD.setDateTimeCreated("2015-07-15T16:47:26");
+		TEST_NISO_IMAGE_MD.setImageProducer("BnF");
+		TEST_NISO_IMAGE_MD.setScanningSoftware("Archiver");
+		TEST_NISO_IMAGE_MD.setOrientation(1);
+		TEST_NISO_IMAGE_MD.setSamplingFrequencyUnit(2);
+		TEST_NISO_IMAGE_MD.setXSamplingFrequency(new Rational(564, 1));
+		TEST_NISO_IMAGE_MD.setYSamplingFrequency(new Rational(564, 1));
+		TEST_NISO_IMAGE_MD.setBitsPerSample(new int[] { 8, 8, 8 });
+		TEST_NISO_IMAGE_MD.setSamplesPerPixel(3);
+		TEST_NISO_IMAGE_MD.setWhitePointXValue(new Rational(1343036288L,
+				4294967295L));
+		TEST_NISO_IMAGE_MD.setWhitePointYValue(new Rational(1343036288L,
+				4294967295L));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesRedX(new Rational(2748779008L,
+				4294967295L));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesRedY(new Rational(1417339264L,
+				4294967295L));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesGreenX(new Rational(
+				1288490240L, 4294967295L));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesGreenY(new Rational(
+				2576980480L, 4294967295L));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesBlueX(new Rational(644245120L,
+				4294967295L));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesBlueY(new Rational(257698032L,
+				4294967295L));
+
+		// Define the test instance for TextMD to be serialized
+		TEST_TEXTMD = new TextMDMetadata();
+		TEST_TEXTMD.setCharset(TextMDMetadata.CHARSET_UTF8);
+		TEST_TEXTMD.setByte_order(TextMDMetadata.BYTE_ORDER_LITTLE);
+		TEST_TEXTMD.setByte_size("8");
+		TEST_TEXTMD.setCharacter_size("variable");
+		TEST_TEXTMD.setLinebreak(TextMDMetadata.LINEBREAK_LF);
+		TEST_TEXTMD.setMarkup_basis("XML");
+		TEST_TEXTMD.setMarkup_basis_version("1.0");
+		TEST_TEXTMD.setMarkup_language("http://www.web3d.org/specifications/x3d-3.1.xsd");
+		TEST_TEXTMD.setMarkup_language_version("3.1");
+	}
+	
+	@Before
+	public void setUp() throws IOException {
+		// Prepare for a new test
+		outputFile = File.createTempFile("jhove_", ".xml");
+		writer = new PrintWriter(outputFile);
+		handler = new XmlHandler();
+		handler.setWriter(writer);
+	}
+
+	@After
+	public void tearDown() {
+		if (outputFile != null && outputFile.exists()) {
+			outputFile.delete();
+		}
+	}
+
+	@Test
+	public void testShowNisoImageMetadata02() throws IOException {
+		File mix02File = new File(
+				"src/test/resources/xmlHandler/mix02_output.xml");
+		LOGGER.info("testShowNisoImageMetadata02 with file " + mix02File);
+		assertTrue(mix02File.isFile());
+		String expectedMix02 = readXmlFile(mix02File);
+
+		handler.showNisoImageMetadata02(TEST_NISO_IMAGE_MD);
+		handler.close();
+
+		String generatedMix = readXmlFile(outputFile);
+		assertEquals("Mix v0.2 generated not conformant", expectedMix02, generatedMix);
+	}
+
+	@Test
+	public void testShowNisoImageMetadata10() throws IOException {
+		File mix10File = new File(
+				"src/test/resources/xmlHandler/mix10_output.xml");
+		LOGGER.info("testShowNisoImageMetadata10 with file " + mix10File);
+		assertTrue(mix10File.isFile());
+		String expectedMix10 = readXmlFile(mix10File);
+
+		handler.showNisoImageMetadata10(TEST_NISO_IMAGE_MD);
+		handler.close();
+
+		String generatedMix = readXmlFile(outputFile);
+		assertEquals("Mix v1.0 generated not conformant", expectedMix10, generatedMix);
+	}
+
+	@Test
+	public void testShowNisoImageMetadata20() throws IOException {
+		File mix20File = new File(
+				"src/test/resources/xmlHandler/mix20_output.xml");
+		LOGGER.info("testShowNisoImageMetadata20 with file " + mix20File);
+		assertTrue(mix20File.isFile());
+		String expectedMix20 = readXmlFile(mix20File);
+
+		handler.showNisoImageMetadata20(TEST_NISO_IMAGE_MD);
+		handler.close();
+
+		String generatedMix = readXmlFile(outputFile);
+		assertEquals("Mix v2.0 generated not conformant", expectedMix20, generatedMix);
+	}
+
+	@Test
+	public void testShowTextMDMetadata() throws IOException {
+		File textMD30File = new File(
+				"src/test/resources/xmlHandler/text30_output.xml");
+		LOGGER.info("testShowTextMDMetadata with file " + textMD30File);
+		assertTrue(textMD30File.isFile());
+		String expectedText30 = readXmlFile(textMD30File);
+
+		handler.showTextMDMetadata(TEST_TEXTMD);
+		handler.close();
+
+		String generatedTextMD = readXmlFile(outputFile);
+		assertEquals("TextMD v3.0 generated not conformant", expectedText30, generatedTextMD);
+	}
+
+	private String readXmlFile(File f) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		try {
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+		} finally {
+			br.close();
+		}
+		return sb.toString().replaceAll("\\s+", " ");
+	}
+}
