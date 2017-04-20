f8b8e12cb629aa16b1c3b7e3c285cd61cc20d7c2
Thomas Ledoux <tledouxfr@gmail.com>
2017-04-20 18:26:34 +0200

    Correct codacy advices

--- jhove-core/src/test/java/edu/harvard/hul/ois/jhove/handler/XmlHandlerTest.java
+++ jhove-core/src/test/java/edu/harvard/hul/ois/jhove/handler/XmlHandlerTest.java
@@ -23,67 +23,86 @@ import edu.harvard.hul.ois.jhove.TextMDMetadata;
 
 @RunWith(JUnit4.class)
 public class XmlHandlerTest {
-    private static final Logger LOGGER = Logger.getLogger (XmlHandlerTest.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(XmlHandlerTest.class
+			.getName());
 
-	File outputFile;
-	PrintWriter writer;
-	XmlHandler handler;
+	private static final int EXPECTED_IMAGE_WIDTH = 8192;
+	private static final int EXPECTED_IMAGE_RESOLUTION = 564;
+	private static final int EXPECTED_LAST_BYTE = 255;
+	private static final int EXPECTED_BYTE_SIZE = 8;
+	private static final int EXPECTED_NUMBER_OF_BYTES = 3;
+	private static final long EXPECTED_DENOMINATOR = 4294967295L;
+	private static final long EXPECTED_WP_NUMENATOR = 1343036288L;
+	private static final long EXPECTED_REDX_NUMENATOR = 2748779008L;
+	private static final long EXPECTED_REDY_NUMENATOR = 1417339264L;
+	private static final long EXPECTED_GREENX_NUMENATOR = 1288490240L;
+	private static final long EXPECTED_GREENY_NUMENATOR = 2576980480L;
+	private static final long EXPECTED_BLUEX_NUMENATOR = 644245120L;
+	private static final long EXPECTED_BLUEY_NUMENATOR = 257698032L;
+
+	private File outputFile;
+	private PrintWriter writer;
+	private XmlHandler handler;
 
 	protected static NisoImageMetadata TEST_NISO_IMAGE_MD;
 	protected static TextMDMetadata TEST_TEXTMD;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws IOException {
 		// Define the test instance for NisoImageMetadata to be serialized
 		TEST_NISO_IMAGE_MD = new NisoImageMetadata();
 		TEST_NISO_IMAGE_MD.setByteOrder(NisoImageMetadata.BYTEORDER[1]);
 		TEST_NISO_IMAGE_MD.setCompressionScheme(1);
-		TEST_NISO_IMAGE_MD.setImageWidth(8192);
-		TEST_NISO_IMAGE_MD.setImageLength(8192);
+		TEST_NISO_IMAGE_MD.setImageWidth(EXPECTED_IMAGE_WIDTH);
+		TEST_NISO_IMAGE_MD.setImageLength(EXPECTED_IMAGE_WIDTH);
 		TEST_NISO_IMAGE_MD.setColorSpace(2);
 		Rational r0 = new Rational(0, 1);
-		Rational r255 = new Rational(255, 1);
-		TEST_NISO_IMAGE_MD.setReferenceBlackWhite(new Rational[] { r0, r255, r0,
-				r255, r0, r255 });
+		Rational r255 = new Rational(EXPECTED_LAST_BYTE, 1);
+		TEST_NISO_IMAGE_MD.setReferenceBlackWhite(new Rational[] { r0, r255,
+				r0, r255, r0, r255 });
 		TEST_NISO_IMAGE_MD.setDateTimeCreated("2015-07-15T16:47:26");
 		TEST_NISO_IMAGE_MD.setImageProducer("BnF");
 		TEST_NISO_IMAGE_MD.setScanningSoftware("Archiver");
 		TEST_NISO_IMAGE_MD.setOrientation(1);
 		TEST_NISO_IMAGE_MD.setSamplingFrequencyUnit(2);
-		TEST_NISO_IMAGE_MD.setXSamplingFrequency(new Rational(564, 1));
-		TEST_NISO_IMAGE_MD.setYSamplingFrequency(new Rational(564, 1));
-		TEST_NISO_IMAGE_MD.setBitsPerSample(new int[] { 8, 8, 8 });
-		TEST_NISO_IMAGE_MD.setSamplesPerPixel(3);
-		TEST_NISO_IMAGE_MD.setWhitePointXValue(new Rational(1343036288L,
-				4294967295L));
-		TEST_NISO_IMAGE_MD.setWhitePointYValue(new Rational(1343036288L,
-				4294967295L));
-		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesRedX(new Rational(2748779008L,
-				4294967295L));
-		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesRedY(new Rational(1417339264L,
-				4294967295L));
+		TEST_NISO_IMAGE_MD.setXSamplingFrequency(new Rational(
+				EXPECTED_IMAGE_RESOLUTION, 1));
+		TEST_NISO_IMAGE_MD.setYSamplingFrequency(new Rational(
+				EXPECTED_IMAGE_RESOLUTION, 1));
+		TEST_NISO_IMAGE_MD.setBitsPerSample(new int[] { EXPECTED_BYTE_SIZE,
+				EXPECTED_BYTE_SIZE, EXPECTED_BYTE_SIZE });
+		TEST_NISO_IMAGE_MD.setSamplesPerPixel(EXPECTED_NUMBER_OF_BYTES);
+		TEST_NISO_IMAGE_MD.setWhitePointXValue(new Rational(EXPECTED_WP_NUMENATOR,
+				EXPECTED_DENOMINATOR));
+		TEST_NISO_IMAGE_MD.setWhitePointYValue(new Rational(EXPECTED_WP_NUMENATOR,
+				EXPECTED_DENOMINATOR));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesRedX(new Rational(
+				EXPECTED_REDX_NUMENATOR, EXPECTED_DENOMINATOR));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesRedY(new Rational(
+				EXPECTED_REDY_NUMENATOR, EXPECTED_DENOMINATOR));
 		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesGreenX(new Rational(
-				1288490240L, 4294967295L));
+				EXPECTED_GREENX_NUMENATOR, EXPECTED_DENOMINATOR));
 		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesGreenY(new Rational(
-				2576980480L, 4294967295L));
-		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesBlueX(new Rational(644245120L,
-				4294967295L));
-		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesBlueY(new Rational(257698032L,
-				4294967295L));
+				EXPECTED_GREENY_NUMENATOR, EXPECTED_DENOMINATOR));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesBlueX(new Rational(
+				EXPECTED_BLUEX_NUMENATOR, EXPECTED_DENOMINATOR));
+		TEST_NISO_IMAGE_MD.setPrimaryChromaticitiesBlueY(new Rational(
+				EXPECTED_BLUEY_NUMENATOR, EXPECTED_DENOMINATOR));
 
 		// Define the test instance for TextMD to be serialized
 		TEST_TEXTMD = new TextMDMetadata();
 		TEST_TEXTMD.setCharset(TextMDMetadata.CHARSET_UTF8);
 		TEST_TEXTMD.setByte_order(TextMDMetadata.BYTE_ORDER_LITTLE);
-		TEST_TEXTMD.setByte_size("8");
+		TEST_TEXTMD.setByte_size("" + EXPECTED_BYTE_SIZE);
 		TEST_TEXTMD.setCharacter_size("variable");
 		TEST_TEXTMD.setLinebreak(TextMDMetadata.LINEBREAK_LF);
 		TEST_TEXTMD.setMarkup_basis("XML");
 		TEST_TEXTMD.setMarkup_basis_version("1.0");
-		TEST_TEXTMD.setMarkup_language("http://www.web3d.org/specifications/x3d-3.1.xsd");
+		TEST_TEXTMD
+				.setMarkup_language("http://www.web3d.org/specifications/x3d-3.1.xsd");
 		TEST_TEXTMD.setMarkup_language_version("3.1");
 	}
-	
+
 	@Before
 	public void setUp() throws IOException {
 		// Prepare for a new test
@@ -112,7 +131,8 @@ public class XmlHandlerTest {
 		handler.close();
 
 		String generatedMix = readXmlFile(outputFile);
-		assertEquals("Mix v0.2 generated not conformant", expectedMix02, generatedMix);
+		assertEquals("Mix v0.2 generated not conformant", expectedMix02,
+				generatedMix);
 	}
 
 	@Test
@@ -127,7 +147,8 @@ public class XmlHandlerTest {
 		handler.close();
 
 		String generatedMix = readXmlFile(outputFile);
-		assertEquals("Mix v1.0 generated not conformant", expectedMix10, generatedMix);
+		assertEquals("Mix v1.0 generated not conformant", expectedMix10,
+				generatedMix);
 	}
 
 	@Test
@@ -142,7 +163,8 @@ public class XmlHandlerTest {
 		handler.close();
 
 		String generatedMix = readXmlFile(outputFile);
-		assertEquals("Mix v2.0 generated not conformant", expectedMix20, generatedMix);
+		assertEquals("Mix v2.0 generated not conformant", expectedMix20,
+				generatedMix);
 	}
 
 	@Test
@@ -157,7 +179,8 @@ public class XmlHandlerTest {
 		handler.close();
 
 		String generatedTextMD = readXmlFile(outputFile);
-		assertEquals("TextMD v3.0 generated not conformant", expectedText30, generatedTextMD);
+		assertEquals("TextMD v3.0 generated not conformant", expectedText30,
+				generatedTextMD);
 	}
 
 	private String readXmlFile(File f) throws IOException {
