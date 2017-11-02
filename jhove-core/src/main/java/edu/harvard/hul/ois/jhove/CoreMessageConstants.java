f180ce0c140c8cf539f10845212f44acb56ffe95
Carl Wilson <carl@openplanetsfoundation.org>
2017-11-02 14:18:06 +0000

    FIX - Consistent use of full stop.

--- jhove-core/src/main/java/edu/harvard/hul/ois/jhove/CoreMessageConstants.java
+++ jhove-core/src/main/java/edu/harvard/hul/ois/jhove/CoreMessageConstants.java
@@ -27,8 +27,8 @@ public enum CoreMessageConstants {
 	public static final String EXC_PROP_CLSS_INCMPT = "Incompatible class for property: ";
 	public static final String EXC_SCL_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "Scalar.";
 	public static final String EXC_MAP_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "Map.";
-	public static final String EXC_SET_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "Set";
-	public static final String EXC_LIST_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "List";
+	public static final String EXC_SET_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "Set.";
+	public static final String EXC_LIST_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "List.";
 	public static final String EXC_TEMP_FILE_CRT = "Cannot create temporary file";
 	public static final String EXC_URI_CONV_FAIL = "Cannot convert URI to URL: ";
 	public static final String EXC_URL_NOT_FND = "URL not found: ";
