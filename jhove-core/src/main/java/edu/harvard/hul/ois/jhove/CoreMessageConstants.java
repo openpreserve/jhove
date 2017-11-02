2804553d1d47ed05fb169aa01d93f454b50b77ea
Carl Wilson <carl@openplanetsfoundation.org>
2017-11-02 14:17:08 +0000

    FIX - Using EXC_PROP_CLSS_INCMPT part message consistently.

--- jhove-core/src/main/java/edu/harvard/hul/ois/jhove/CoreMessageConstants.java
+++ jhove-core/src/main/java/edu/harvard/hul/ois/jhove/CoreMessageConstants.java
@@ -23,12 +23,12 @@ public enum CoreMessageConstants {
 	public static final String EXC_MODL_INST_FAIL = "Cannot instantiate module: ";
 	public static final String EXC_PRV_CNSTRCT = "Entered private constructor for: ";
 	public static final String EXC_PROP_VAL_NULL = "Null value not permitted or property: ";
-	public static final String EXC_PROP_CLSS_INCMPT = "Incompatible class for property: ";
 	public static final String EXC_SAX_PRSR_MISS = "SAX parser not found: ";
+	public static final String EXC_PROP_CLSS_INCMPT = "Incompatible class for property: ";
 	public static final String EXC_SCL_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "Scalar.";
-	public static final String EXC_MAP_PROP_CLSS_INCMPT = "Incompatible class for property: Map.";
-	public static final String EXC_SET_PROP_CLSS_INCMPT = "Incompatible class for property: Set";
-	public static final String EXC_LIST_PROP_CLSS_INCMPT = "Incompatible class for property: List";
+	public static final String EXC_MAP_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "Map.";
+	public static final String EXC_SET_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "Set";
+	public static final String EXC_LIST_PROP_CLSS_INCMPT = EXC_PROP_CLSS_INCMPT + "List";
 	public static final String EXC_TEMP_FILE_CRT = "Cannot create temporary file";
 	public static final String EXC_URI_CONV_FAIL = "Cannot convert URI to URL: ";
 	public static final String EXC_URL_NOT_FND = "URL not found: ";
