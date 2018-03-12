566005f3cf91d9678b1f8b4194ed7439ad3e420b
Carl Wilson <carl@openplanetsfoundation.org>
2018-03-12 13:12:08 +0000

    Dedicated PDF Header parsing class.

--- jhove-modules/src/main/java/edu/harvard/hul/ois/jhove/module/pdf/PdfHeader.java
+++ jhove-modules/src/main/java/edu/harvard/hul/ois/jhove/module/pdf/PdfHeader.java
@@ -5,9 +5,6 @@ package edu.harvard.hul.ois.jhove.module.pdf;
 
 import java.io.IOException;
 
-import edu.harvard.hul.ois.jhove.ErrorMessage;
-import edu.harvard.hul.ois.jhove.RepInfo;
-
 /**
  * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
  *         <a href="https://github.com/carlwilson">carlwilson AT github</a>
@@ -15,52 +12,74 @@ import edu.harvard.hul.ois.jhove.RepInfo;
  */
 
 public final class PdfHeader {
-	public static final String PDF_VER1_HEADER_PREFIX = "PDF-1.";
-	public static final String PDF_SIG_HEADER = "%" + PDF_VER1_HEADER_PREFIX;
-	public static final String POSTSCRIPT_HEADER_PREFIX = "!PS-Adobe-";
-	
-	private final int majorVersion;
-	private final int minorVersion;
+	public static final String PDF_VER1_HEADER_PREFIX = "PDF-1."; //$NON-NLS-1$
+	public static final String PDF_SIG_HEADER = "%" + PDF_VER1_HEADER_PREFIX; //$NON-NLS-1$
+	public static final String POSTSCRIPT_HEADER_PREFIX = "!PS-Adobe-"; //$NON-NLS-1$
 
-	
+	private final String versionString;
+	private final boolean isPdfACompilant;
+
 	/**
 	 * 
 	 */
-	private PdfHeader(final int majorVersion, final int minorVersion) {
-		this.majorVersion = majorVersion;
-		this.minorVersion = minorVersion;
+	private PdfHeader(final String versionString,
+			final boolean isPdfaCompliant) {
+		this.versionString = versionString;
+		this.isPdfACompilant = isPdfaCompliant;
 	}
 
-	public static boolean parseHeader(final RepInfo info, final Parser _parser, final String _name) throws IOException {
+	/**
+	 * @return the version string parsed from the PDF Header
+	 */
+	public String getVersionString() {
+		return this.versionString;
+	}
+
+	/**
+	 * @return true if the header is considered PDF/A compliant, otherwise false
+	 */
+	public boolean isPdfACompliant() {
+		return this.isPdfACompilant;
+	}
+
+	/**
+	 * Factory method for {@link PdfHeader} that parses a new instance using the
+	 * supplied {@link Parser} instance.
+	 * 
+	 * @param _parser
+	 *            the {@link Parser} instance that will be used to parse header
+	 *            details
+	 * @return a new {@link PdfHeader} instance derived using the supplied
+	 *         {@link Parser} or <code>null</code> when no header could be found
+	 *         and parsed.
+	 */
+	public static PdfHeader parseHeader(final Parser _parser) {
 		Token token = null;
 		String value = null;
+		boolean isPdfACompliant = false;
+		String version = null;
 
 		/* Parse file header. */
-
-		boolean foundSig = false;
 		for (;;) {
 			if (_parser.getOffset() > 1024) {
-				break;
+				return null;
 			}
 			try {
 				token = null;
 				token = _parser.getNext(1024L);
 			} catch (IOException ee) {
-				break;
+				return null;
 			} catch (Exception e) {
 			} // fall through
+
 			if (token == null) {
-				break;
+				return null;
 			}
 			if (token instanceof Comment) {
 				value = ((Comment) token).getValue();
 				if (value.indexOf(PDF_VER1_HEADER_PREFIX) == 0) {
-					foundSig = true;
-					_version = value.substring(4, 7);
-					/*
-					 * If we got this far, take note that the signature is OK.
-					 */
-					info.setSigMatch(_name);
+					version = value.substring(4, 7);
+					isPdfACompliant = true;
 					break;
 				}
 				// The implementation notes (though not the spec)
@@ -70,25 +89,15 @@ public final class PdfHeader {
 					// PostScript signature.
 					int n = value.indexOf(PDF_VER1_HEADER_PREFIX);
 					if (n >= 11) {
-						foundSig = true;
-						_version = value.substring(n + 4);
-						// However, this is not PDF-A compliant.
-						_pdfACompliant = false;
-						info.setSigMatch(_name);
+						version = value.substring(n + 4);
 						break;
 					}
 				}
 			}
-
-			// If we don't find it right at the beginning, we aren't
-			// PDF/A compliant.
-			_pdfACompliant = false;
 		}
-		if (!foundSig) {
-			info.setWellFormed(false);
-			info.setMessage(new ErrorMessage(
-					MessageConstants.ERR_PDF_HEADER_MISSING, 0L));
-			return false;
+
+		if (version == null) {
+			return null;
 		}
 		// Check for PDF/A conformance. The next item must be
 		// a comment with four characters, each greater than 127
@@ -103,13 +112,13 @@ public final class PdfHeader {
 				}
 			}
 			if (ctlcnt < 4) {
-				_pdfACompliant = false;
+				isPdfACompliant = false;
 			}
 		} catch (Exception e) {
 			// Most likely a ClassCastException on a non-comment
-			_pdfACompliant = false;
+			isPdfACompliant = false;
 		}
-		return true;
+		return new PdfHeader(version, isPdfACompliant);
 	}
 
 }
