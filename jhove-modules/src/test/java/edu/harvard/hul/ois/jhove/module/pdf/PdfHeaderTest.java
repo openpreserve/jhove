de4bccbf52ed685f5b7af8b7399522aff59cf461
Carl Wilson <carl@openplanetsfoundation.org>
2018-03-18 14:48:25 +0000

    REFACT - PDF Header Tests - now uses `TestUtils`; and - basic JavaDoc.

--- jhove-modules/src/test/java/edu/harvard/hul/ois/jhove/module/pdf/PdfHeaderTest.java
+++ jhove-modules/src/test/java/edu/harvard/hul/ois/jhove/module/pdf/PdfHeaderTest.java
@@ -17,6 +17,9 @@ import edu.harvard.hul.ois.jhove.RepInfo;
 import edu.harvard.hul.ois.jhove.module.PdfModule;
 
 /**
+ * Tests for the {@link PdfHeader} class and for JHOVE's PDF Header validation
+ * capabilities.
+ *
  * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
  *         <a href="https://github.com/carlwilson">carlwilson AT github</a>
  * @version 0.1 Created 13 Mar 2018:11:28:10
@@ -39,60 +42,25 @@ public class PdfHeaderTest {
 	}
 
 	/**
-	 * Test method for
-	 * {@link edu.harvard.hul.ois.jhove.module.pdf.PdfHeader#getVersionString()}.
+	 * Test method for {@link edu.harvard.hul.ois.jhove.module.pdf.PdfHeader}.
+	 * Ensures that a valid PDF passes.
 	 */
 	@Test
-	public final void testMinorVersion()
-			throws URISyntaxException {
-		File validMinor = new File(
-				PdfHeaderTest.class.getResource(minimalPdfPath).toURI());
-
-		RepInfo info = parseTestFile(validMinor);
-
-		// Major version number < 8 should be well formed
-		assertEquals("Should be well formed.", RepInfo.TRUE, info.getWellFormed());
-		// Major version number < 8 should be invalid
-		assertEquals("Should be valid.", RepInfo.TRUE, info.getValid());
+	public final void testMinorVersion() throws URISyntaxException {
+		TestUtils.testValidateResource(this.module, minimalPdfPath,
+				RepInfo.TRUE, RepInfo.TRUE, null);
 	}
 
 	/**
 	 * Test method for
 	 * {@link edu.harvard.hul.ois.jhove.module.pdf.PdfHeader#getVersionString()}.
+	 * Ensures that a file with %PDF-1.9 is invalid.
 	 */
 	@Test
-	public final void testInvalidMinorVersion()
-			throws URISyntaxException {
-		File invalidMajor = new File(
-				PdfHeaderTest.class.getResource(invalidMinorPath).toURI());
+	public final void testInvalidMinorVersion() throws URISyntaxException {
+		TestUtils.testValidateResource(this.module, invalidMinorPath,
+				RepInfo.TRUE, RepInfo.FALSE,
+				MessageConstants.ERR_PDF_MINOR_INVALID);
 
-		RepInfo info = parseTestFile(invalidMajor);
-
-		// Major version number > 7 should be well formed
-		assertEquals("Should be well formed.", RepInfo.TRUE, info.getWellFormed());
-		assertEquals("Should NOT be valid.", RepInfo.FALSE, info.getValid());
-	}
-
-	private RepInfo parseTestFile(final File toTest) {
-		RepInfo info = new RepInfo(toTest.getName());
-		RandomAccessFile raf = null;
-		try {
-			raf = new RandomAccessFile(toTest, "r");
-			this.module.parse(raf, info);
-		} catch (FileNotFoundException excep) {
-			excep.printStackTrace();
-			fail("Couldn't find file to test: " + toTest.getName());
-		} catch (IOException excep) {
-			excep.printStackTrace();
-			fail("IOException Reading: " + toTest.getName());
-		}
-		try {
-			if (raf != null) {
-				raf.close();
-			}
-		} catch (@SuppressWarnings("unused") IOException excep) {
-			// We don't care about this..
-		}
-		return info;
 	}
 }
