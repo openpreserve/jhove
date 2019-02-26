0ac3e18aa1dbab4d5a683aa2a2af304bc673d461
Carl Wilson <carl@openplanetsfoundation.org>
2019-02-26 11:16:05 +0000

    FEAT - Error IDs - made `Message` class immutable; - added member for `ids`; and - added appropriate constructors for backward compatibility.

--- jhove-core/src/main/java/edu/harvard/hul/ois/jhove/InfoMessage.java
+++ jhove-core/src/main/java/edu/harvard/hul/ois/jhove/InfoMessage.java
@@ -6,57 +6,121 @@
 package edu.harvard.hul.ois.jhove;
 
 /**
- *  This class encapsulates an informational message from a Module, giving
- *  information (not necessarily a problem)
- *  about the content being analyzed or the way that Jhove
- *  deals with it. 
+ * This class encapsulates an informational message from a Module, giving
+ * information (not necessarily a problem)
+ * about the content being analyzed or the way that Jhove
+ * deals with it.
  */
-public class InfoMessage
-    extends Message
-{
-    /******************************************************************
-     * CLASS CONSTRUCTOR.
-     ******************************************************************/
+public class InfoMessage extends Message {
+	/******************************************************************
+	 * CLASS CONSTRUCTOR.
+	 ******************************************************************/
 
-    /** 
-     *  Create an InfoMessage.
-     *  @param  message   Human-readable string giving the information.
-     */
-    public InfoMessage (String message)
-    {
-	super (message);
-    }
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param message
+	 *            Human-readable string giving the information.
+	 */
+	public InfoMessage(String message) {
+		super(message);
+	}
 
-    /** 
-     *  Create an InfoMessage.
-     *  @param  message   Human-readable string giving the information.
-     *  @param  offset    The offset in the file relevant to the
-     *                    situation being described
-     */
-    public InfoMessage (String message, long offset)
-    {
-	super (message, offset);
-    }
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string giving the information.
+	 */
+	public InfoMessage(int id, String message) {
+		super(id, message);
+	}
 
-    /** 
-     *  Create an InfoMessage.
-     *  @param  message   Human-readable string giving the information.
-     *  @param  subMessage Human-readable additional information.
-     */
-    public InfoMessage (String message, String subMessage)
-    {
-        super (message, subMessage);
-    }
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param message
+	 *            Human-readable string giving the information.
+	 * @param offset
+	 *            The offset in the file relevant to the
+	 *            situation being described
+	 */
+	public InfoMessage(String message, long offset) {
+		super(message, offset);
+	}
 
-    /** 
-     *  Create an InfoMessage.
-     *  @param  message   Human-readable string giving the information.
-     *  @param  subMessage Human-readable additional information.
-     *  @param  offset    The offset in the file relevant to the
-     *                    situation being described
-     */
-    public InfoMessage (String message, String subMessage, long offset)
-    {
-        super (message, subMessage, offset);
-    }
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string giving the information.
+	 * @param offset
+	 *            The offset in the file relevant to the
+	 *            situation being described
+	 */
+	public InfoMessage(int id, String message, long offset) {
+		super(id, message, offset);
+	}
+
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param message
+	 *            Human-readable string giving the information.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 */
+	public InfoMessage(String message, String subMessage) {
+		super(message, subMessage);
+	}
+
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string giving the information.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 */
+	public InfoMessage(int id, String message, String subMessage) {
+		super(id, message, subMessage);
+	}
+
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param message
+	 *            Human-readable string giving the information.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 * @param offset
+	 *            The offset in the file relevant to the
+	 *            situation being described
+	 */
+	public InfoMessage(String message, String subMessage, long offset) {
+		super(message, subMessage, offset);
+	}
+
+	/**
+	 * Create an InfoMessage.
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string giving the information.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 * @param offset
+	 *            The offset in the file relevant to the
+	 *            situation being described
+	 */
+	public InfoMessage(int id, String message, String subMessage, long offset) {
+		super(id, message, subMessage, offset);
+	}
 }
--- jhove-core/src/main/java/edu/harvard/hul/ois/jhove/Message.java
+++ jhove-core/src/main/java/edu/harvard/hul/ois/jhove/Message.java
@@ -6,116 +6,200 @@
 package edu.harvard.hul.ois.jhove;
 
 /**
- *  This class encapsulates a String to be displayed. 
+ * This class encapsulates a String to be displayed.
  */
-public abstract class Message
-{
-    /******************************************************************
-     * PUBLIC CLASS FIELDS.
-     ******************************************************************/
+public abstract class Message {
+	/******************************************************************
+	 * PUBLIC CLASS FIELDS.
+	 ******************************************************************/
 
-    /** Value indicating a null offset. */
-    public static final long NULL = -1;
+	/** Value indicating a null offset. */
+	public static final long NULL = -1;
 
-    /******************************************************************
-     * PRIVATE INSTANCE FIELDS.
-     ******************************************************************/
+	public static final int UNKNOWN_ID = -1;
 
-    /** Message text. */
-    protected final String _message;
-    
-    /** Additional information. */
-    protected final String _subMessage;
-    
-    /** Byte offset to which message applies. */
-    protected final long _offset;
+	/******************************************************************
+	 * PRIVATE INSTANCE FIELDS.
+	 ******************************************************************/
 
-    /******************************************************************
-     * CLASS CONSTRUCTOR.
-     ******************************************************************/
+	/** Error message ID number */
+	protected final int _id;
 
-    /**
-     *  Create a Message.  This constructor cannot be invoked directly,
-     *                     since Message is abstract.
-     *  @param  message   Human-readable string.
-     */
-    protected Message (String message)
-    {
-	this(message, null);
-    }
+	/** Message text. */
+	protected final String _message;
 
-    /**
-     *  Create a Message.  This constructor cannot be invoked directly,
-     *                     since Message is abstract.  The second argument
-     *                     adds secondary details to the primary message;
-     *                     the message will typically be displayed in the
-     *                     form "message:subMessage".
-     *  @param  message    Human-readable string.
-     *  @param  subMessage Human-readable additional information.
-     */
-    protected Message (String message, String subMessage)
-    {
-        this(message, subMessage, NULL);
-    }
+	/** Additional information. */
+	protected final String _subMessage;
 
-    /**
-     *  Create a Message.  This constructor cannot be invoked directly,
-     *                     since Message is abstract.  The second argument
-     *                     adds secondary details to the primary message;
-     *                     the message will typically be displayed in the
-     *                     form "message:subMessage".
-     *  @param  message    Human-readable string.
-     *  @param  offset     Byte offset associated with the message.
-     */
-    protected Message (String message, long offset)
-    {
-        this(message, null, offset);
-    }
+	/** Byte offset to which message applies. */
+	protected final long _offset;
 
-    /**
-     *  Create a Message.  This constructor cannot be invoked directly,
-     *                     since Message is abstract.  The second argument
-     *                     adds secondary details to the primary message;
-     *                     the message will typically be displayed in the
-     *                     form "message:subMessage".
-     *  @param  message    Human-readable string.
-     *  @param  subMessage Human-readable additional information.
-     *  @param  offset     Byte offset associated with the message.
-     */
-    protected Message (String message, String subMessage, long offset)
-    {
-        this._message = message;
-        this._subMessage = subMessage;
-        this._offset = offset;
-    }
+	/******************************************************************
+	 * CLASS CONSTRUCTOR.
+	 ******************************************************************/
 
-    /******************************************************************
-     * PUBLIC INSTANCE METHODS.
-     *
-     * Accessor methods.
-     ******************************************************************/
+	/**
+	 * Create a Message with an unknown id (for backward compatibility).
+	 * This constructor cannot be invoked directly,
+	 * since Message is abstract.
+	 * 
+	 * @param message
+	 *            Human-readable string.
+	 */
+	protected Message(String message) {
+		this(message, UNKNOWN_ID);
+	}
 
-    /**
-     *  Get the message string.
-     */
-    public String getMessage ()
-    {
-        return _message;
-    }
+	/**
+	 * Create a Message with an id. This constructor cannot be invoked directly,
+	 * since Message is abstract.
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string.
+	 */
+	protected Message(int id, String message) {
+		this(id, message, null, NULL);
+	}
 
-    /**
-     *  Get the submessage string.
-     */
-    public String getSubMessage ()
-    {
-        return _subMessage;
-    }
+	/**
+	 * Create a Message. This constructor cannot be invoked directly,
+	 * since Message is abstract. The second argument
+	 * adds secondary details to the primary message;
+	 * the message will typically be displayed in the
+	 * form "message:subMessage".
+	 * 
+	 * @param message
+	 *            Human-readable string.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 */
+	protected Message(String message, String subMessage) {
+		this(UNKNOWN_ID, message, subMessage);
+	}
 
-    /**
-     *  Return the offset to which the information is related.
-     */
-    public long getOffset ()
-    {
-	return _offset;
-    }
+	/**
+	 * Create a Message. This constructor cannot be invoked directly,
+	 * since Message is abstract. The second argument
+	 * adds secondary details to the primary message;
+	 * the message will typically be displayed in the
+	 * form "message:subMessage".
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 */
+	protected Message(int id, String message, String subMessage) {
+		this(id, message, subMessage, NULL);
+	}
+
+	/**
+	 * Create a Message. This constructor cannot be invoked directly,
+	 * since Message is abstract. The second argument
+	 * adds secondary details to the primary message;
+	 * the message will typically be displayed in the
+	 * form "message:subMessage".
+	 * 
+	 * @param message
+	 *            Human-readable string.
+	 * @param offset
+	 *            Byte offset associated with the message.
+	 */
+	protected Message(String message, long offset) {
+		this(UNKNOWN_ID, message, offset);
+	}
+
+	/**
+	 * Create a Message. This constructor cannot be invoked directly,
+	 * since Message is abstract. The second argument
+	 * adds secondary details to the primary message;
+	 * the message will typically be displayed in the
+	 * form "message:subMessage".
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string.
+	 * @param offset
+	 *            Byte offset associated with the message.
+	 */
+	protected Message(int id, String message, long offset) {
+		this(id, message, null, offset);
+	}
+
+	/**
+	 * Create a Message. This constructor cannot be invoked directly,
+	 * since Message is abstract. The second argument
+	 * adds secondary details to the primary message;
+	 * the message will typically be displayed in the
+	 * form "message:subMessage".
+	 * 
+	 * @param message
+	 *            Human-readable string.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 * @param offset
+	 *            Byte offset associated with the message.
+	 */
+	protected Message(String message, String subMessage, long offset) {
+		this(UNKNOWN_ID, message, subMessage, offset);
+	}
+
+	/**
+	 * Create a Message. This constructor cannot be invoked directly,
+	 * since Message is abstract. The second argument
+	 * adds secondary details to the primary message;
+	 * the message will typically be displayed in the
+	 * form "message:subMessage".
+	 * 
+	 * @param id
+	 *            Unique ID (within the module) for the message
+	 * @param message
+	 *            Human-readable string.
+	 * @param subMessage
+	 *            Human-readable additional information.
+	 * @param offset
+	 *            Byte offset associated with the message.
+	 */
+	protected Message(int id, String message, String subMessage, long offset) {
+		this._id = id;
+		this._message = message;
+		this._subMessage = subMessage;
+		this._offset = offset;
+	}
+
+	/******************************************************************
+	 * PUBLIC INSTANCE METHODS.
+	 *
+	 * Accessor methods.
+	 ******************************************************************/
+
+	/**
+	 * Get the message string.
+	 */
+	public String getMessage() {
+		return _message;
+	}
+
+	/**
+	 * Get the submessage string.
+	 */
+	public String getSubMessage() {
+		return _subMessage;
+	}
+
+	/**
+	 * Return the offset to which the information is related.
+	 */
+	public long getOffset() {
+		return _offset;
+	}
+
+	public int getId() {
+		return this._id;
+	}
 }
