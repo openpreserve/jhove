83f9c3d8cb8fde236d8281a82e512626b0e9c6b2
Carl Wilson <carl@openplanetsfoundation.org>
2019-05-08 15:47:49 +0100

    REL - TIFF Module 1.9.2 - updated TIFF module release details; - bumped TIFF module patch version; - updated TIFF module Maven dependencies; - updated TIFF module includes in izpack config; and - updated TIFF version for test baseline creation.

--- jhove-bbt/scripts/create-1.22-target.sh
+++ jhove-bbt/scripts/create-1.22-target.sh
@@ -54,60 +54,6 @@ echo "Executing baseline update"
 # Simply copy baseline for now we're not making any changes
 cp -R "${candidateRoot}" "${targetRoot}"
 
-# # BYTESTREAM Module
-# #
-# # New version details
-# # In the JHOVE Audit file
-# find "${targetRoot}" -type f -name "audit.jhove.xml" -exec sed -i 's/^   <module release="1.3">BYTESTREAM<\/module>$/   <module release="1.4">BYTESTREAM<\/module>/' {} \;
-# # and the results files
-# find "${targetRoot}" -type f -name "*.jhove.xml" -exec sed -i 's%<reportingModule release="1.3" date="2007-04-10">BYTESTREAM%<reportingModule release="1.4-RC" date="2019-03-05">BYTESTREAM%' {} \;
-#
-# # ASCII Module
-# #
-# # New version details
-# # In the JHOVE Audit file
-# find "${targetRoot}" -type f -name "audit.jhove.xml" -exec sed -i 's/^   <module release="1.3">ASCII-hul<\/module>$/   <module release="1.4">ASCII-hul<\/module>/' {} \;
-# # In the ASCII Module Audit file
-# find "${targetRoot}" -type f -name "audit-ASCII-hul.jhove.xml" -exec sed -i 's%>2006-09-05</date>%>2019-03-05</date>%' {} \;
-# find "${targetRoot}" -type f -name "audit-ASCII-hul.jhove.xml" -exec sed -i 's/>1.3<\/release>$/>1.4-RC<\/release>/' {} \;
-# find "${targetRoot}" -type f -name "audit-ASCII-hul.jhove.xml" -exec sed -i 's/2003-2007/2003-2015/' {} \;
-# find "${targetRoot}" -type f -name "audit-ASCII-hul.jhove.xml" -exec sed -i 's/College. Released under/College. Copyright 2015-2019 by the Open Preservation Foundation. Version 1.4 onwards developed by Open Preservation Foundation. Released under/' {} \;
-# # In the README file
-# find "${targetRoot}" -type f -name "README.jhove.xml" -exec sed -i 's%<reportingModule release="1.3" date="2006-09-05">ASCII%<reportingModule release="1.4" date="2019-03-05">ASCII%' {} \;
-# # Replace in the results files
-# find "${targetRoot}" -type f -name "*.txt.jhove.xml" -exec sed -i 's%<reportingModule release="1.3" date="2006-09-05">ASCII%<reportingModule release="1.4" date="2019-03-05">ASCII%' {} \;
-# # Reporting changes
-# # FIX ASCII Module TAB code
-# find "${targetRoot}" -type f -name "control.txt.jhove.xml" -exec sed -i 's%TAB (0x09)%HT (0x09)%' {} \;
-#
-# # PDF Module
-# #
-# # New version details
-# # In the JHOVE Audit file
-# find "${targetRoot}" -type f -name "audit.jhove.xml" -exec sed -i 's/^   <module release="1.11">PDF-hul<\/module>$/   <module release="1.12">PDF-hul<\/module>/' {} \;
-# # In the PDF Module Audit file
-# find "${targetRoot}" -type f -name "audit-PDF-hul.jhove.xml" -exec sed -i 's%>2018-03-29</date>%>2019-03-05</date>%' {} \;
-# find "${targetRoot}" -type f -name "audit-PDF-hul.jhove.xml" -exec sed -i 's/>1.11<\/release>$/>1.12<\/release>/' {} \;
-# # Replace in the results files
-# find "${targetRoot}" -type f -name "*.pdf.jhove.xml" -exec sed -i 's%<reportingModule release="1.11" date="2018-03-29">PDF%<reportingModule release="1.12" date="2019-03-05">PDF%' {} \;
-# # In the README file
-# find "${targetRoot}" -type f -name "README.jhove.xml" -exec sed -i 's%<reportingModule release="1.11" date="2018-03-29">PDF%<reportingModule release="1.12" date="2019-03-05">PDF%' {} \;
-#
-# # Fix Class Cast Error in PDF Module see https://github.com/openpreserve/jhove/issues/173
-# if [[ -f "${candidateRoot}/regression/modules/PDF-hul/class-cast.pdf.jhove.xml" ]]; then
-# 	echo "Copying #173 fix"
-# 	cp "${candidateRoot}/regression/modules/PDF-hul/class-cast.pdf.jhove.xml" "${targetRoot}/regression/modules/PDF-hul/"
-# fi
-#
-# # UTF-8 Module
-# #
-# # New version details
-# # In the JHOVE Audit file
-# find "${targetRoot}" -type f -name "audit.jhove.xml" -exec sed -i 's/^   <module release="1.6">UTF8-hul<\/module>$/   <module release="1.7">UTF8-hul<\/module>/' {} \;
-# # In the ASCII Module Audit file
-# find "${targetRoot}" -type f -name "audit-UTF8-hul.jhove.xml" -exec sed -i 's%>2014-07-18</date>%>2019-03-05</date>%' {} \;
-# find "${targetRoot}" -type f -name "audit-UTF8-hul.jhove.xml" -exec sed -i 's/>1.6<\/release>$/>1.7<\/release>/' {} \;
-# # In the README file
-# find "${targetRoot}" -type f -name "README.jhove.xml" -exec sed -i 's%<reportingModule release="1.6" date="2014-07-18">UTF8%<reportingModule release="1.7" date="2019-03-05">UTF8%' {} \;
-# # Replace in the results files
-# find "${targetRoot}" -type f -name "*.txt.jhove.xml" -exec sed -i 's%<reportingModule release="1.6" date="2014-07-18">UTF8%<reportingModule release="1.7" date="2019-03-05">UTF8%' {} \;
+# # TIFF Module fixes
+find "${targetRoot}" -type f -name "audit.jhove.xml" -exec sed -i 's/^   <module release="1.9.1">TIFF-hul<\/module>$/   <module release="1.9.2">TIFF-hul<\/module>/' {} \;
+find "${targetRoot}" -type f -name "audit-TIFF-hul.jhove.xml" -exec sed -i 's%>2019-04-17</date>%>2019-04-30</date>%' {} \;
