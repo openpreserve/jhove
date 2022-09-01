686a72ba43127ed6f4b5ff50f759d6d14557c41b
Karen Hanson <karenhanson77@gmail.com>
2022-09-01 18:10:15 -0400

    Manage infinite loop in parseIFDChain
    Resolves stackOverflowError caused by an incorrect next offset value that caused the code to loop back on previously processed IFDs until it errors. The code does not currently detect when IFDs with the same offset are visted more than once. This fix breaks out after 50 cycles - copying the solution for `parseIFDs()`.

--- jhove-modules/tiff-hul/src/main/java/edu/harvard/hul/ois/jhove/module/TiffModule.java
+++ jhove-modules/tiff-hul/src/main/java/edu/harvard/hul/ois/jhove/module/TiffModule.java
@@ -1234,6 +1234,10 @@ public class TiffModule extends ModuleBase {
             ifd.setThumbnail(true);
         }
         list.add(ifd);
+        
+        if (list.size() > 50) {
+            throw new TiffException(MessageConstants.TIFF_HUL_60);
+        }
 
         if (ifd instanceof TiffIFD) {
             TiffIFD tifd = (TiffIFD) ifd;
