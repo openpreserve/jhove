aa7da01a43897590a94ee4fdcf6f3953e642e174
Bernhard Hampel-Waffenthal <bernhard.hampel-waffenthal@obvsg.at>
2022-12-19 20:05:05 +0100

    update PDF Name prefix list and sort alphabetically

--- jhove-modules/pdf-hul/src/main/java/edu/harvard/hul/ois/jhove/module/pdf/PdfStrings.java
+++ jhove-modules/pdf-hul/src/main/java/edu/harvard/hul/ois/jhove/module/pdf/PdfStrings.java
@@ -105,78 +105,80 @@ public class PdfStrings
      *  Maintained in: https://github.com/adobe/pdf-names-list
      * . */
     public final static List<String> PREFIXNAMESREGISTY = new ArrayList<String>() {{
-	add("ADBE"); // Adobe
-	add("ABCp"); //Zandent Ltd
-	add("ADLB"); //Adlib Publishing Systems
-	add("AJIc"); //Aji, LLC
-	add("ASGJ"); //AcroScript
-	add("BECI"); //BEC GmbH & Co KG
-	add("BFOO"); //BFO
-	add("bPRO"); //BiPRO e.V.
-	add("CALS"); //Callas Software gmbh
-	add("cfca"); //China Finacial Certification Authority Co.Ltd
-	add("CFCA"); //China Finacial Certification Authority Co.Ltd
-	add("CIP4"); //International Cooperation for the Integration of Process in Prepress, Press and Postpress Association
-	add("CLWN"); //pdfclown.org
-	add("CRDF"); //Autonomy Cardiff
-	add("diTe"); //disphere tech GmbH
-	add("DMSK"); //DMS One Zrt.
-	add("DUFF"); //Duff Johnson Consulting
-	add("Esko"); //Esko Software bv
-	add("EVSC"); //Evansco, LLC
-	add("FICL"); //Enfocus BV
-	add("FNBC"); //ALSTOM (Switzerland) Ltd
-	add("FOPN"); //FileOpen Systems Inc.
-	add("GeoD"); //Mapthematics LLC
-	add("GFSw"); //Goofyfootsoftware
-	add("GGSL"); //Global Graphics
-	add("GLAU"); //AS207960 Cyfyngedig
-	add("glTF"); //ISO (via the 3D PDF Consortium)
-	add("GTSm"); //ISO TC130/WG2 as described in ISO 21812
-	add("GURG"); //Gurnet Group LLC
-	add("GWG_"); //Ghent Workgroup
-	add("HEBD"); //H-E-B Grocery
-	add("HYBR"); //HYBRID Software Development NV
-	add("ICTI"); //iCerti
-	add("iPDF"); //InteractivePDF.org
-	add("ISO_"); //ISO (via the 3D PDF Consortium)
-	add("ITXT"); //1T3XT BVBA
-	add("K3SD"); //Kanrikogaku Kenkyusho, Ltd.,  Meguro Office
-	add("KWSQ"); //Kawseq Consulting Pty Ltd
-	add("LTUd"); //Office of the Chief Archivist of Lithuania
-	add("MKLx"); //Michael Klink
-	add("MSFT"); //Microsoft Corporation
-	add("MSMO"); //Mortgage Industry Standards Maintenance Organization
-	add("MTSJ"); //Mekentosj BV
-	add("NORM"); //Normex s.r.o.
-	add("NPTC"); //nepatec GmbH
-	add("nptc"); //nepatec GmbH
-	add("OOPS"); //JNJ
-	add("PBAD"); //Instituto Nacional de Tecnologia da Informação
-	add("pdfa"); //PDF Association
-	add("PDTH"); //PDF Thingys
-	add("PIER"); //Pierre Choutet
-	add("Plib"); //PDFlib GmbH
-	add("PTEX"); //PRAGMA ADE
-	add("S4Cx"); //Sense4code s.r.o.
-	add("SCIN"); //Scinaptic Communications
-	add("SETA"); //Setasign
-	add("sjda"); //Andrea Vacandio, Sejda.org
-	add("slns"); //OneSpan
-	add("SNLF"); //SNL Financial, LC
-	add("SOFH"); //SOFHA Gmbh
-	add("SRIT"); //Reindl-IT
-	add("STEP"); //ISO (via the 3D PDF Consortium)
-	add("TGSI"); //Telegenisys Inc.
-	add("TMSL"); //Timeslice Ltd
-	add("USCT"); //Administrative Office of the U.S. Courts
-	add("VSMA"); //Visma Software International AS
-	add("wgss"); //Wacom Co, Ltd
-	add("WGSS"); //Wacom Co, Ltd
-	add("WKTM"); //Walters Kluwer TeamMate
-	add("WRPC"); //UAB "Superita"
-	add("ZETO"); //ZETO Sp. z o.o. w Lublinie
-
+        add("_Y4Y"); // Yin4Yang BV
+        add("ABCp"); // Zandent Ltd
+        add("ADBE"); // Adobe Inc.
+        add("ADLB"); // Adlib Publishing Systems
+        add("AJIc"); // Aji, LLC
+        add("ASGJ"); // AcroScript
+        add("BECI"); // BEC GmbH & Co KG
+        add("BFOO"); // BFO
+        add("bPRO"); // BiPRO e.V.
+        add("CALS"); // Callas Software gmbh
+        add("cfca"); // China Finacial Certification Authority Co.Ltd
+        add("CFCA"); // China Finacial Certification Authority Co.Ltd
+        add("CIP4"); // International Cooperation for the Integration of Process in Prepress, Press and Postpress Association
+        add("CLWN"); // pdfclown.org
+        add("CRDF"); // Autonomy Cardiff
+        add("diTe"); // disphere tech GmbH
+        add("DMSK"); // DMS One Zrt.
+        add("DUFF"); // Duff Johnson Consulting
+        add("Esko"); // Esko Software bv
+        add("EVSC"); // Evansco, LLC
+        add("FICL"); // Enfocus BV
+        add("FNBC"); // ALSTOM (Switzerland) Ltd
+        add("FOPN"); // FileOpen Systems Inc.
+        add("FOXI"); // FOXIT SOFTWARE INC.
+        add("GeoD"); // Mapthematics LLC
+        add("GFSw"); // Goofyfootsoftware
+        add("GGSL"); // Global Graphics
+        add("GLAU"); // AS207960 Cyfyngedig
+        add("glTF"); // ISO (via the 3D PDF Consortium)
+        add("GTSm"); // ISO TC130/WG2 as described in ISO 21812
+        add("GURG"); // Gurnet Group LLC
+        add("GWG_"); // Ghent Workgroup
+        add("HEBD"); // H-E-B Grocery
+        add("HYBR"); // HYBRID Software Development NV
+        add("ICTI"); // iCerti
+        add("iPDF"); // InteractivePDF.org
+        add("ISO_"); // ISO (via the 3D PDF Consortium)
+        add("ITXT"); // 1T3XT BVBA
+        add("K3SD"); // Kanrikogaku Kenkyusho, Ltd.,  Meguro Office
+        add("KWSQ"); // Kawseq Consulting Pty Ltd
+        add("LTUd"); // Office of the Chief Archivist of Lithuania
+        add("MKLx"); // Michael Klink
+        add("MSFT"); // Microsoft Corporation
+        add("MSMO"); // Mortgage Industry Standards Maintenance Organization
+        add("MTSJ"); // Mekentosj BV
+        add("NORM"); // Normex s.r.o.
+        add("NPTC"); // nepatec GmbH
+        add("nptc"); // nepatec GmbH
+        add("OOPS"); // JNJ
+        add("PBAD"); // Instituto Nacional de Tecnologia da Informação
+        add("pdfa"); // PDF Association
+        add("PDTH"); // PDF Thingys
+        add("PIER"); // Pierre Choutet
+        add("Plib"); // PDFlib GmbH
+        add("PTEX"); // PRAGMA ADE
+        add("S4Cx"); // Sense4code s.r.o.
+        add("SCIN"); // Scinaptic Communications
+        add("SETA"); // Setasign
+        add("sjda"); // Andrea Vacandio, Sejda.org
+        add("slns"); // OneSpan
+        add("SNLF"); // SNL Financial, LC
+        add("SOFH"); // SOFHA Gmbh
+        add("SRIT"); // Reindl-IT
+        add("STEP"); // ISO (via the 3D PDF Consortium)
+        add("TGSI"); // Telegenisys Inc.
+        add("TMSL"); // Timeslice Ltd
+        add("USCT"); // Administrative Office of the U.S. Courts
+        add("VSMA"); // Visma Software International AS
+        add("wgss"); // Wacom Co, Ltd
+        add("WGSS"); // Wacom Co, Ltd
+        add("WKTM"); // Walters Kluwer TeamMate
+        add("WRPC"); // UAB "Superita"
+        add("XMPi"); // XMPie Ltd.
+        add("ZETO"); // ZETO Sp. z o.o. w Lublinie
     }};
     
     /** A private constructor just to make sure nobody
