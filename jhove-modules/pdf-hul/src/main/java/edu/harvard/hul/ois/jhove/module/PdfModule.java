4033529e674ed8658d1ced94ee5eb247802f9834
Martin Hoppenheit <martin@hoppenheit.info>
2025-02-25 15:39:19 +0100

    Simplify XMP processing in PDF

--- jhove-modules/pdf-hul/src/main/java/edu/harvard/hul/ois/jhove/module/PdfModule.java
+++ jhove-modules/pdf-hul/src/main/java/edu/harvard/hul/ois/jhove/module/PdfModule.java
@@ -2141,48 +2141,38 @@ public class PdfModule extends ModuleBase {
             if (metadata == null) {
                 return true; // Not required
             }
-            // PdfDictionary metaDict = metadata.getDict ();
-
-            // Create an InputSource to feed the parser.
+            PdfXMPSource src = new PdfXMPSource(metadata, getFile(), "UTF-8");
             SAXParserFactory factory = SAXParserFactory.newInstance();
             factory.setNamespaceAware(true);
             XMLReader parser = factory.newSAXParser().getXMLReader();
-            PdfXMPSource src = new PdfXMPSource(metadata, getFile());
             XMPHandler handler = new XMPHandler();
             parser.setContentHandler(handler);
             parser.setErrorHandler(handler);
-
-            // We have to parse twice. The first time, we may get
-            // an encoding change as part of an exception thrown. If this
-            // happens, we create a new InputSource with the encoding, and
-            // continue.
-            try {
-                parser.parse(src);
-                _xmpProp = src.makeProperty();
-            } catch (SAXException se) {
-                String msg = se.getMessage();
-                if (msg != null && msg.startsWith(ENCODING_PREFIX)) {
-                    String encoding = msg.substring(5);
-                    try {
-                        src = new PdfXMPSource(metadata, getFile(), encoding);
-                        parser.parse(src);
-                        _xmpProp = src.makeProperty();
-                    } catch (UnsupportedEncodingException uee) {
-                        _logger.log(Level.INFO,
-                                "Attempt to use explicit encoding to parse XMP metadata failed.",
-                                uee);
-                        throw new PdfInvalidException(
-                                MessageConstants.PDF_HUL_100); // PDF-HUL-100
-                    }
-                }
-            }
-
+            parser.parse(src);
+            _xmpProp = src.makeProperty();
+        } catch (UnsupportedEncodingException uee) {
+            // TODO Might be raised by PdfXMPSource but won't because "UTF-8" is
+            // quite OK. So why catch this at all? Rather check above whether
+            // the input is valid UTF-8.
+            _logger.log(Level.INFO,
+                    "Attempt to use explicit encoding to parse XMP metadata failed.",
+                    uee);
+            info.setMessage(new ErrorMessage(MessageConstants.PDF_HUL_100, // PDF-HUL-100
+                    _parser.getOffset()));
+            info.setValid(false);
+            return false;
+        } catch (SAXException se) {
+            info.setMessage(new ErrorMessage(MessageConstants.PDF_HUL_101, // PDF-HUL-101
+                    _parser.getOffset()));
+            info.setValid(false);
+            return false;
         } catch (PdfException e) {
             e.disparage(info);
             info.setMessage(new ErrorMessage(e.getJhoveMessage(), _parser.getOffset()));
             // Continue parsing if it's only invalid
             return (e instanceof PdfInvalidException);
         } catch (Exception e) {
+            // TODO Really raise this for every kind of generic exception?
             info.setMessage(new ErrorMessage(MessageConstants.PDF_HUL_101, // PDF-HUL-101
                     _parser.getOffset()));
             info.setValid(false);
