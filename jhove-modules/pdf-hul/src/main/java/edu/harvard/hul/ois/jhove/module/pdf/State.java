c297073343ab3a600456889032bb6ee0f025e6da
Bernhard Hampel-Waffenthal <bernhard.hampel-waffenthal@obvsg.at>
2025-04-18 16:55:20 +0200

    only process bytes and use proper charset-based decoding for detected encoding of literal strings

--- jhove-modules/pdf-hul/src/main/java/edu/harvard/hul/ois/jhove/module/pdf/State.java
+++ jhove-modules/pdf-hul/src/main/java/edu/harvard/hul/ois/jhove/module/pdf/State.java
@@ -43,8 +43,7 @@ public class State
     public static final State LITERAL = new State ("LITERAL");
     public static final State LITERAL_FE = new State ("LITERAL_FE");
     public static final State LITERAL_PDF = new State ("LITERAL_PDF");
-    public static final State LITERAL_UTF16_1 = new State ("LITERAL_UTF16_1");
-    public static final State LITERAL_UTF16_2 = new State ("LITERAL_UTF16_2");
+    public static final State LITERAL_UTF16 = new State("LITERAL_UTF16");
     public static final State NAME = new State ("NAME");
     public static final State NUMERIC = new State ("NUMERIC");
     public static final State STREAM = new State ("STREAM");
